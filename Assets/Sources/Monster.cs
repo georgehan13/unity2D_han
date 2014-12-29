eae38f4680c138319fe6973f0d24b8216b5b8cc4
george <darkblue_b4@hotmail.com>
2014-12-29 21:40:56 +0900

    add monstser script

---
+++ Assets/Sources/Monster.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Monster : MonoBehaviour 
+{
+	Animator animator = null;
+
+	void Start () 
+	{
+	
+	}
+
+	void Update () 
+	{
+	
+	}
+}
