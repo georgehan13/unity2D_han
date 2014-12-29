3df93655cae8bcb57d361e07b0d84daa89ae97c3
george <darkblue_b4@hotmail.com>
2014-12-29 22:37:55 +0900

    monster state change .... use enum MonsterState... in script

--- Assets/Sources/Monster.cs
+++ Assets/Sources/Monster.cs
@@ -3,17 +3,31 @@ using System.Collections;
 
 public class Monster : MonoBehaviour 
 {
+	enum MonsterState
+	{
+		None = -1,
+		Idle,
+		Damage,
+		Die
+	}
+
 	Animator animator = null;
+	MonsterState state = MonsterState.None;
 
 	void Start () 
 	{
 		animator = GetComponent<Animator>();
+		state = MonsterState.Idle;
+		animator.SetInteger("State", (int)state);
+
 		//animator.SetBool ("Check", true);
+
 
 	}
 
 	void Update () 
 	{
+		/*
 		if(Input.GetMouseButtonDown(0)==true)
 		{
 			animator.SetBool ("Check", true);
@@ -22,6 +36,8 @@ public class Monster : MonoBehaviour
 		{
 			animator.SetBool ("Check", false);
 		}
-	
+		*/
+
+
 	}
 }
