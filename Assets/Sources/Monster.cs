11daf45b6c92c5e94ce793bbc99698efb59d82ad
george <darkblue_b4@hotmail.com>
2014-12-29 22:16:30 +0900

    mousedown change check parameter

--- Assets/Sources/Monster.cs
+++ Assets/Sources/Monster.cs
@@ -8,12 +8,20 @@ public class Monster : MonoBehaviour
 	void Start () 
 	{
 		animator = GetComponent<Animator>();
-		animator.SetBool ("Check", true);
+		//animator.SetBool ("Check", true);
 
 	}
 
 	void Update () 
 	{
+		if(Input.GetMouseButtonDown(0)==true)
+		{
+			animator.SetBool ("Check", true);
+		}
+		else if (Input.GetMouseButtonDown(1)==true)
+		{
+			animator.SetBool ("Check", false);
+		}
 	
 	}
 }
