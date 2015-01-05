27e0f46387beb7d94e053bd2b2680a175f7afe14
george <darkblue_b4@hotmail.com>
2015-01-05 21:42:40 +0900

    몬스터 배열하기 전 과정

---
+++ Assets/Sources/MonsterManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonsterManager : MonoBehaviour 
+{
+	public GameObject enemy = null;
+	float interval = 0;
+	float intervalMax = 3;
+	float startX = -2;
+
+
+	void Start () 
+	{
+	
+	}
+	
+	void Update () 
+	{
+		interval += Time.deltaTime;
+		if(interval > intervalMax)
+		{
+			/* 5마리 만든다??
+			GameObject obj1 = Instantiate(enemy) as GameObject
+			GameObject obj2 = Instantiate(enemy) as GameObject
+			GameObject obj3 = Instantiate(enemy) as GameObject
+			GameObject obj4 = Instantiate(enemy) as GameObject
+			GameObject obj5 = Instantiate(enemy) as GameObject
+			*/
+
+			for(int i = 0;i < 5; i++)
+			{
+				GameObject obj = Instantiate(enemy) as GameObject
+				obj.transform.ImagePosition = 
+			}
+			interval = 0;
+		}
+	}
+}
