b5756193c301e04eab5213866fe117c750c1abaf
george <darkblue_b4@hotmail.com>
2015-01-08 20:19:38 +0900

    메테오라이트 매니저 - 코루틴 추가

---
+++ Assets/Sources/MeteoriteManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeteoriteManager : MonoBehaviour 
+{
+
+	void Start () 
+	{
+		StartCoroutine(Call ());
+	}
+
+	IEnumerator Call()
+	{
+		yield return new WaitForSeconds(3);
+
+		print("Call !!!!!");
+	}
+}
