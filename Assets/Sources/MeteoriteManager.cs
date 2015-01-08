f7ffa11c5deae0892c7cd25a6ccd8175eec56406
george <darkblue_b4@hotmail.com>
2015-01-08 20:32:54 +0900

    코루틴 예시

--- Assets/Sources/MeteoriteManager.cs
+++ Assets/Sources/MeteoriteManager.cs
@@ -6,13 +6,40 @@ public class MeteoriteManager : MonoBehaviour
 
 	void Start () 
 	{
-		StartCoroutine(Call ());
+		//StartCoroutine(Call ());
+
+		IEnumerator e = Call2();
+		while(e.MoveNext())
+		{
+			print(e.Current);
+		}
 	}
 
+	IEnumerator Call2()
+	{
+		yield return 1;
+
+		yield return "kim";
+
+		yield return 0.1f;
+	}
+
+
+	/*
 	IEnumerator Call()
 	{
 		yield return new WaitForSeconds(3);
 
 		print("Call !!!!!");
+
+		yield return new WaitForSeconds(2);
+		
+		print("Call2 !!!!!");
+
+		yield return new WaitForSeconds(1);
+		
+		print("Call3 !!!!!");
+
 	}
+	*/
 }
