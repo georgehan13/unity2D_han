c734b77d6d7381650f4319d33c8e93385ff2edc6
george <darkblue_b4@hotmail.com>
2015-01-08 21:43:40 +0900

    웹에서 파일 받아오기 - 코루틴 예제

---
+++ Assets/Sources/TestWeb.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestWeb : MonoBehaviour 
+{
+	WWW www = null;
+	bool isDownLoad = false;
+
+
+	void Start() 
+	{
+		StartCoroutine(Call ());
+	}
+
+	IEnumerator Call()
+	{
+		www = new WWW("http://180.70.94.180/test.png");
+		yield return www;
+
+		if(www.isDone == true)
+		{
+			isDownLoad = true;
+			print("완료!!!");
+		}
+	}
+
+	void Update()
+	{
+		if(isDownLoad == false)
+		{
+			print ("Progress" + www.progress);
+		}
+	}
+}
