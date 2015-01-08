b62ed826f1fafa4065efaa9b8d205f1a55c1ced9
george <darkblue_b4@hotmail.com>
2015-01-08 20:47:37 +0900

    alarm 오브젝트 호출

--- Assets/Sources/MeteoriteManager.cs
+++ Assets/Sources/MeteoriteManager.cs
@@ -3,43 +3,46 @@ using System.Collections;
 
 public class MeteoriteManager : MonoBehaviour 
 {
+	public GameObject alarm = null;
+	public GameObject meteorite = null;
+	public GameObject tail = null;
+
 
 	void Start () 
 	{
-		//StartCoroutine(Call ());
+		StartCoroutine(Call ());
 
+		/*
 		IEnumerator e = Call2();
 		while(e.MoveNext())
 		{
 			print(e.Current);
 		}
+		*/
 	}
 
+
+
+
+	IEnumerator Call()
+	{
+		float time = Random.Range (2.0f, 5.0f);
+
+		yield return new WaitForSeconds(time);
+
+		GameObject alarmObj = Instantiate(alarm) as GameObject;
+		alarmObj.transform.position = this.transform.position;
+	
+	}
+
+	/*
 	IEnumerator Call2()
 	{
 		yield return 1;
-
+		
 		yield return "kim";
-
+		
 		yield return 0.1f;
-	}
-
-
-	/*
-	IEnumerator Call()
-	{
-		yield return new WaitForSeconds(3);
-
-		print("Call !!!!!");
-
-		yield return new WaitForSeconds(2);
-		
-		print("Call2 !!!!!");
-
-		yield return new WaitForSeconds(1);
-		
-		print("Call3 !!!!!");
-
 	}
 	*/
 }
