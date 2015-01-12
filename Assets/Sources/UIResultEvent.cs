67dfe1e1575d5be41d482ea0270e03a2376f8687
george <darkblue_b4@hotmail.com>
2015-01-12 21:53:22 +0900

    result title event

---
+++ Assets/Sources/UIResultEvent.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIResultEvent : MonoBehaviour 
+{
+	public void Retry()
+	{
+		Application.LoadLevel("Game");
+	}
+
+	public void ToTitle()
+	{
+		Application.LoadLevel("Title");
+	}
+}
