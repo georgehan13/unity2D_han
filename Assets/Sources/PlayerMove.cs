03882322494551879ebbefdc092a93a45c967d59
george <darkblue_b4@hotmail.com>
2015-01-15 22:39:05 +0900

    터치기반 // 외벽충돌

--- Assets/Sources/PlayerMove.cs
+++ Assets/Sources/PlayerMove.cs
@@ -3,8 +3,26 @@ using System.Collections;
 
 public class PlayerMove : MonoBehaviour 
 {
-	float speed = 5;
+	float speed = 350;
 
+	void FixedUpdate()
+	{
+		float dirX = 0;
+		if(Input.GetMouseButton (0) == true)
+		{
+			dirX = Input.GetAxis ("Mouse X");
+		}
+
+		if(Input.GetMouseButtonDown(0) == true)
+		{
+			dirX = 0; // 처음 실행시 초기와
+		}
+
+		float velocityX = dirX * speed * Time.deltaTime;
+		this.rigidbody2D.velocity = new Vector2(velocityX, 0);
+	}
+
+	/*
 	void Update () 
 	{
 		float dirX = Input.GetAxis("Horizontal");
@@ -17,4 +35,5 @@ public class PlayerMove : MonoBehaviour
 
 		//print (dir);
 	}
+	*/
 }
